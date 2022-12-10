08fdb23c32f7952aef6ee0c06f598176a8ef7021
SkyeVaccu <839994148@qq.com>
2022-12-10 21:57:15 +0800

    fix the websocket bug

--- lib/util/websocket/websocket_client.dart
+++ lib/util/websocket/websocket_client.dart
@@ -5,12 +5,16 @@ import 'package:skye_utils/util/object_util.dart';
 import 'package:skye_utils/extension/string_extension.dart';
 import 'package:skye_utils/util/serialize/serialize_util.dart';
 import 'package:skye_utils/util/skye_table.dart';
+import 'package:skye_utils/util/timer_util.dart';
 import 'package:skye_utils/util/websocket/websocket_client_operator.dart';
 import 'package:skye_utils/util/websocket/websocket_client_receiver.dart';
 import 'package:skye_utils/util/websocket/websocket_client_sender.dart';
 import 'package:skye_utils/util/websocket/websocket_package.dart';
 
 ///this is the websocket client object ,you can create an websocket client fast by this
+///the ping and pong form is packaged into the WebSocket , you can see that class if you want the detail
+///if the time pass the heartBeatPeriod ,the client don't receive the pong ,the client will be seen as closed
+///So we start the inspector to handle this function
 class WebSocketClient {
   //websocket server host
   String? host;
@@ -22,6 +26,8 @@ class WebSocketClient {
   String protocol;
   //ping and pong ,heart beat period, default value is 15
   int heartBeatPeriod;
+  //reconnected threshold ,if the time pass the value ,we will started to reconnected
+  int notResponseThreshold;
   //the uri param after the url
   Map<String, String>? urlParams;
   //the params in the headers
@@ -31,11 +37,13 @@ class WebSocketClient {
   //listener the message event
   void Function(String)? messageListener;
   //listener the close event
-  void Function(String)? closeListener;
+  void Function()? closeListener;
   //listener the error event
   void Function(Exception)? errorListener;
   //the true WebSocket object
   WebSocket? _webSocket;
+  //the status of the websocket client
+  WebSocketClientStatus webSocketClientStatus = WebSocketClientStatus.WAIT;
   //it's the table store all receivers
   final SkyeTable<String, String, List<WebSocketClientReceiver>> _receiverTable = new SkyeTable();
   //it's the table store all senders
@@ -45,6 +53,7 @@ class WebSocketClient {
     required this.port,
     this.protocol = "ws",
     this.heartBeatPeriod = 15,
+    this.notResponseThreshold = 3,
     this.urlParams,
     this.openParams,
     this.openListener,
@@ -60,6 +69,7 @@ class WebSocketClient {
     required this.uri,
     this.protocol = "ws",
     this.heartBeatPeriod = 15,
+    this.notResponseThreshold = 3,
     this.urlParams,
     this.openParams,
     this.openListener,
@@ -110,9 +120,43 @@ class WebSocketClient {
     _webSocket?.add(SerializeUtil.serialize(webSocketPackage));
   }
 
+  ///close the websocket
+  ///@param code : the close code
+  ///@param reason : the close reason
+  void close({int? code, String? reason}) {
+    //close the websocket
+    _webSocket?.close(code, reason);
+    Log.e("close the webSocketClient");
+    //call the close listener
+    closeListener?.call();
+  }
+
   ///connect the websocket sever
   ///@return : the connected WebSocketClient object
   void connect() async {
+    _connect();
+    //start the inspector
+    TimerUtil.periodExecuteTask(
+        intervalSeconds: heartBeatPeriod * notResponseThreshold,
+        function: (timer) async {
+          //the _webSocket has closed
+          if (_webSocket!.closeCode != null) {
+            //we need to keep the connection
+            if (webSocketClientStatus == WebSocketClientStatus.OPEN) {
+              //restart connect it
+              _connect();
+            }
+            //we close the connection by myself
+            else if (webSocketClientStatus == WebSocketClientStatus.CLOSE) {
+              //cancel the inspector
+              timer.cancel();
+            }
+          }
+        });
+  }
+
+  ///the true connect method
+  void _connect() async {
     if (ObjectUtil.isAnyEmpty([host, port]) && ObjectUtil.isEmpty(uri)) {
       Log.e("websocket server information can't be empty");
       throw "websocket server information can't be empty";
@@ -141,6 +185,8 @@ class WebSocketClient {
         //set the ping interval , the client will automatically send the ping message
         // you can see the source code annotation to get the detail
         ..pingInterval = Duration(seconds: heartBeatPeriod);
+      //set the webStatus is open
+      webSocketClientStatus = WebSocketClientStatus.OPEN;
       //call the open listener
       openListener?.call();
       //print the message
@@ -162,4 +208,14 @@ class WebSocketClient {
       });
     }
   }
+}
+
+///the webSocketClient status
+enum WebSocketClientStatus {
+  //just create id but not connect to the server
+  WAIT,
+  //the connection is open
+  OPEN,
+  //the connection is close
+  CLOSE,
 }
