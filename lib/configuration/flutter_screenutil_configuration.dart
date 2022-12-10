5601b529cedfbf44d6565fb21bab1975c6a120de
SkyeVaccu <839994148@qq.com>
2022-12-10 20:55:39 +0800

    fix the dependency bug

--- lib/configuration/flutter_screenutil_configuration.dart
+++ lib/configuration/flutter_screenutil_configuration.dart
@@ -29,10 +29,12 @@ class FlutterScreenUtilConfiguration {
       currentSize = isPortrait ? Size(1080, 1920) : Size(1920, 1080);
     }
     ScreenUtil.init(
-      context,
-      designSize: currentSize!, // 设计图尺寸
-      splitScreenMode: true,
-      minTextAdapt: true,
-    ); // 屏幕方向
+        BoxConstraints(
+          maxWidth: MediaQuery.of(context).size.width, //屏幕宽度
+          maxHeight: MediaQuery.of(context).size.height, //屏幕高度
+        ),
+        designSize: currentSize!, // 设计图尺寸
+        orientation: orientation,
+        context: context); // 屏幕方向
   }
 }
